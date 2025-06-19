7b1c800a79bad78e0b633feeb1d88f6a08076e5f
Yasra Chandio <chandioyasra@gmail.com>
2025-06-19 17:51:07 -0400

    all apps without lib, build, log

---
+++ Symbolic-Manipulation/Assets/cuberecorder.cs
@@ -0,0 +1,290 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+using System;
+using UnityEngine.Serialization;
+using System.Globalization;
+/*using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+using UnityEngine.Serialization;
+using System;
+using System.Globalization;
+using System.Diagnostics;
+
+//using Microsoft.MixedReality.Toolkit;
+//using System.Diagnostics;
+//using System.ComponentModel;
+//using System.Globalization;
+//using System.Linq;*/
+
+public class cuberecorder : MonoBehaviour
+{
+  
+    public GameObject cube;
+    private Material cubeMaterial;
+    private Vector3 initialPosition;
+    private Color originalColor;
+
+    private Color greenColor = Color.green;
+    private Color whiteColor = Color.white;
+    public Color redTimeoutColor = Color.red;
+
+    public float colorChangeInterval = 15f;
+    public float interactionTimeout = 10f; // Interaction timeout in seconds
+    public float redTimeoutDuration = 3f; // Timeout for changing color to red after no interaction in seconds
+
+    private int boxMoveCount = 0; // Count of box movements
+    private int colorChangeToGreenCount = 0; // Count of color changes to green
+    private int colorChangeToWhiteCount = 0; // Count of color changes to white
+    private int colorChangeToRedCount = 0; // Count of color changes to red
+
+    private bool boxMoved = false; // Flag to track box movement
+    private bool interactionRecorded = false; // Flag to track if interaction is recorded
+
+    private bool waitingForInteraction = false; // Flag to track if waiting for interaction timeout
+    private float greenColorStartTime; // Timestamp when the box turned green
+
+    public string fileName;
+    private string dataFolderPath;
+    private StreamWriter trailStreamWriter;
+
+    private Coroutine colorChangeCoroutine; // Coroutine for changing box color
+    private Coroutine interactionCoroutine;
+    private bool interactionStarted = false;
+    private int interactionCount = 0;
+    private int colorChangeCount = 0;
+    private int greenColorChangeCount = 0;
+    private int whiteColorChangeCount = 0;
+
+
+    private float TimeSpan;
+
+    private string appstartTime;
+
+    private Vector3 lastRecordedPosition;
+
+    private float interactionThreshold = 0.1f; // Adjust this value as needed
+    private float lastBoxMovementTime = 0.0f;
+
+    private int colorChangeCounter = 0; // Counter for color changes
+
+    private void Start()
+    {
+        appstartTime = DateTime.Now.ToString("mm:ss.fff");
+        UnityEngine.Debug.Log("App started at" + appstartTime);
+        // Check if the GameObject is assigned in the Unity Editor
+        if (cube != null)
+        {
+            // Access the GameObject and do something with it
+            cube.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            UnityEngine.Debug.Log("GameObject 'cube' is not assigned in the Unity Editor.");
+        }
+
+        cubeMaterial = cube.GetComponent<Renderer>().material;
+        originalColor = cubeMaterial.color;
+
+        // Store initial position of the cube
+        initialPosition = cube.transform.position;
+
+        //StartColorChangeTimer();
+
+        // Create file at the start of the game
+        dataFolderPath = UnityEngine.Application.persistentDataPath;
+
+        // Create file name based on start time
+        string timestamp = System.DateTime.Now.ToString("MMdd_HHmmss");
+        fileName = dataFolderPath + "/" + "box_s_" + timestamp + ".txt";
+
+        // Debug logging to check dataFolderPath and fileName
+        UnityEngine.Debug.Log("Data folder path: " + dataFolderPath);
+        UnityEngine.Debug.Log("File name: " + fileName);
+
+        // Write initial information to the file
+        FileStream mystream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+        //System.IO.File.WriteAllText(fileName, data);
+        trailStreamWriter = new StreamWriter(mystream);
+
+        string data = "Game started at: " + GetCurrentTime();
+        WriteToFile(data);
+
+        // Start the color change routine
+        InvokeRepeating("ChangeColor", colorChangeInterval, colorChangeInterval);
+    }
+
+    private void Update()
+    {
+        // Check distance exceeds for box movement If the  the threshold, consider it as an interaction
+        if (cube.transform.position != initialPosition)
+        {
+            float timenow = GetCurrentTime();
+            if (IsInteracting())
+            {
+                UnityEngine.Debug.Log("inside the IsInteracting");
+                if (waitingForInteraction)
+                {
+                    UnityEngine.Debug.Log("inside waitingforInteraction");
+                    // Record interaction and box movement
+                    RecordInteractionAndMovement();
+                    cubeMaterial.color = whiteColor;
+                    colorChangeToWhiteCount++; // Increment white color count
+                    initialPosition = cube.transform.position;
+                    WriteToFile("Color changed to white at: " + timenow);
+                }
+            }
+
+        }
+
+    }
+
+    // Coroutine to change color after a delay
+    private IEnumerator ChangeColorAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Start the color change routine
+        InvokeRepeating("ChangeColor", colorChangeInterval, colorChangeInterval);
+    }
+
+    // Function to record interaction and movement
+    private void RecordInteractionAndMovement()
+    {
+        // Record interaction start time
+        RecordInteractionStart();
+
+        // Record box movement
+        RecordBoxMovement();
+
+        // Reset boxMoved flag and set interactionRecorded flag
+        //boxMoved = false;
+        interactionRecorded = true;
+    }
+
+    // Function to record box movement
+    private void RecordBoxMovement()
+    {
+        // Record box move time
+        string data = "Box moved at: " + GetCurrentTime();
+        WriteToFile(data);
+
+        // Increment box move count
+        boxMoveCount++;
+    }
+
+    // Function to record interaction start time
+    private void RecordInteractionStart()
+    {
+        // Record interaction start tim
+        string data = "Interaction started at: " + GetCurrentTime();
+        WriteToFile(data);
+
+        // Increment interaction count
+        interactionCount++;
+        waitingForInteraction = false;
+        //interactionRecorded = false;
+    }
+
+    private bool IsInteracting()
+    {
+        // Calculate the distance between the current and last recorded position of the cube
+        float distance = Vector3.Distance(cube.transform.position, initialPosition);
+
+        // If the distance exceeds the threshold, consider it as an interaction
+        if (distance > interactionThreshold)
+        {
+            //boxMoved = true;
+            lastBoxMovementTime = GetCurrentTime();
+            string data = "Change in Distance is:" + distance + " at " + lastBoxMovementTime;
+            // Update the last recorded position
+            initialPosition = cube.transform.position;
+            WriteToFile(data);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void ChangeColor()
+    {
+        // Change the color of the box to green to signal the user to move the box
+        cube.GetComponent<Renderer>().material.color = greenColor;
+        // Record the green color change start time
+        greenColorStartTime = GetCurrentTime();
+
+        // Record the green color change
+        string data = "Color changed to green at: " + greenColorStartTime;
+        WriteToFile(data);
+
+        // Increment green color change count
+        colorChangeToGreenCount++;
+
+        // Start interaction timeout
+        waitingForInteraction = true;
+    }
+
+    private void OnDestroy()
+    {
+        // Record game end time and interaction count
+
+        string data = "Game ended at: " + GetCurrentTime();
+        data += "Total interactions: " + interactionCount + "\n";
+        data += "Total red color changes: " + colorChangeToRedCount + "\n";
+        data += "Total green color changes: " + colorChangeToGreenCount + "\n";
+        data += "Total white color changes: " + colorChangeToWhiteCount + "\n";
+        data += "Total box movements: " + boxMoveCount + "\n";
+        WriteToFile(data);
+
+        // Close the stream writer
+        trailStreamWriter.Close();
+    }
+
+
+    private void WriteToFile(string data)
+    {
+        if (trailStreamWriter != null)
+        {
+            try
+            {
+                trailStreamWriter.WriteLine(data);
+                trailStreamWriter.Flush();
+                UnityEngine.Debug.Log(data);
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError("Error writing to file: " + e.Message);
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("StreamWriter is null.");
+        }
+    }
+
+    private float GetCurrentTime()
+    {
+        return CalculateTimeDifference(appstartTime, DateTime.Now.ToString("mm:ss.fff"));
+    }
+
+
+    public static float CalculateTimeDifference(string timestamp1Str, string timestamp2Str)
+    {
+
+        DateTime timestamp1 = DateTime.ParseExact(timestamp1Str, "mm:ss.fff", CultureInfo.InvariantCulture);
+        DateTime timestamp2 = DateTime.ParseExact(timestamp2Str, "mm:ss.fff", CultureInfo.InvariantCulture);
+
+        TimeSpan timeDifference = timestamp2 - timestamp1;
+        float timeDifferenceInSeconds = (float)timeDifference.TotalMilliseconds / 1000.0f;
+
+        return timeDifferenceInSeconds;
+    }
+}
+
---
+++ Symbolic-Selection/Assets/mainscript.cs
@@ -0,0 +1,421 @@
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+using UnityEngine.Serialization;
+using System.Security.Permissions;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+
+using MixedReality.Toolkit.UX;
+using MixedReality.Toolkit.Subsystems;
+using UnityEngine.XR.Interaction.Toolkit;
+using MixedReality.Toolkit.Input;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using System.Collections.Specialized;
+
+public class mainscript : MonoBehaviour
+{
+
+    public GameObject mainparent;
+    public GameObject[] spheres; // Assign 4 spheres in the order: Red, Green, Blue, Brown
+    public GameObject[] cubes; // Assign cubes ordered to match: Red, Green, Blue, Brown
+    
+    public GameObject greenSphere;
+    public GameObject blueSphere;
+    public GameObject redSphere;
+    public GameObject brownSphere;
+
+    public GameObject greenButton;
+    public GameObject redButton;
+    public GameObject blueButton;
+    public GameObject brownButton;
+
+    public string fileName;
+    private string dataFolderPath;
+    private StreamWriter trailStreamWriter;
+
+    private string appstartTime;
+
+    public float toggleInterval = 2.0f; // Interval in seconds for toggling visibility
+    private float timer;
+
+    public Material highlightMaterial;
+    public Material defaultMaterial;
+
+    private int currentHighlightIndex = 0;
+    private float sphereAppearTime;
+
+    private string highlightedCubeColor;
+    private string movedSphereName;
+    private float touchTime;
+
+   
+    public Renderer targetRenderer;
+    public string materialName;
+    public int currentMaterialIndex = 0;
+
+    private Vector3[] initialPositions; // Array to store the initial positions of cubes
+    private Vector3[] lastRecordedPosition;
+    private float[] distance; 
+    private float distanceThreshold = 0.1f; // Threshold for significant movement
+    private float lastspheremovedIndex;
+    private bool interactionStarted = false;
+    private bool waitingForInteraction = false; // Flag to track if waiting for interaction timeout
+    private int interactionCount = 0;
+
+    private int[] cubeHighlightOrder = { 3, 3, 2, 0, 2, 2, 0, 1 }; // Brown, Brown, Blue, Red, Blue, Blue, Red, Green (indices)
+
+    public bool buttonindexrecorded = false;
+    public string lastprintindex = "";
+    public string lastprintbutton = "";
+    // Sequence of color indices corresponding to spheres (Red = 0, Green = 1, Blue = 2, Brown = 3)
+    private int[] sphereColorSequence = new int[]
+    {
+        0, 1, 2, 3, 0, 1, 3, 2, 0, 2, 1, 3, 0, 2, 3, 1, 0, 3, 1, 2,
+        0, 3, 2, 1, 1, 0, 2, 3, 1, 0, 3, 2, 1, 2, 0, 3, 1, 2, 3, 0,
+        1, 3, 0, 2, 1, 3, 2, 0, 2, 0, 1, 3, 2, 0, 3, 1, 2, 1, 0, 3,
+        2, 1, 3, 0, 2, 3, 0, 1, 2, 3, 1, 0, 3, 0, 1, 2, 3, 0, 2, 1
+    };
+
+    private int currentSphereIndex = 0;
+
+    public void Start()
+    {
+        
+        appstartTime = DateTime.Now.ToString("mm:ss.fff");
+        UnityEngine.Debug.Log("App started at" + appstartTime);
+        dataFolderPath = UnityEngine.Application.persistentDataPath;
+        string timestamp = System.DateTime.Now.ToString("MMdd_HHmmss");
+        fileName = dataFolderPath + "/" + "bubble_s_" + timestamp + ".txt";
+        UnityEngine.Debug.Log("Data folder path: " + dataFolderPath);
+        UnityEngine.Debug.Log("File name: " + fileName);
+
+        // Write initial information to the file
+        FileStream mystream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+        trailStreamWriter = new StreamWriter(mystream);
+
+        string data = "Game started at: " + GetCurrentTime();
+        WriteToFile(data);
+
+        GameObject mainparent = GameObject.Find("Game");
+        spheres = new GameObject[4]; // Re-initialize to be sure
+        cubes = new GameObject[4]; // Re-initialize to be sure
+        // Initialize the array to the same length as the cubes array
+        initialPositions = new Vector3[spheres.Length];
+        lastRecordedPosition = new Vector3[spheres.Length];
+        distance = new float[spheres.Length];
+        //WriteToFile("Initializing spheres array. Length = " + spheres.Length);
+
+        //WriteToFile("Initializing cubes array. Length = " + cubes.Length);
+
+        if (mainparent != null)
+        {
+                // Assign spheres safely
+                string[] sphereNames = { "Game/redbubble", "Game/greenbubble", "Game/bluebubble", "Game/brownbubble" };
+                for (int i = 0; i < spheres.Length; i++)
+                {
+                    //GetChildWithName(mainparent, "greenbubble");
+                    spheres[i] = GameObject.Find(sphereNames[i]);
+                    
+                    if (spheres[i] != null)
+                    {
+                        
+                    //spheres[i].SetActive(false);
+                    //spheres[i].GetComponent<Renderer>().material.color = Color.white;
+                    //WriteToFile("initial position of sphere " + sphereNames[i] +":" + initialPositions[i]);
+                    }
+                    else
+                    {
+                        WriteToFile("Failed to find sphere: " + sphereNames[i]);
+                        //WriteToFile("Sphere at index " + i + " is not assigned.");
+                    }
+                }
+                // Assign cubes safely
+                string[] cubeNames = { "Game/ColorButtons/redbutton", "Game/ColorButtons/greenbutton", "Game/ColorButtons/bluebutton", "Game/ColorButtons/brownbutton" };
+                for (int i = 0; i < cubes.Length; i++)
+                {
+                    cubes[i] = GameObject.Find(cubeNames[i]);
+                    if (cubes[i] == null)
+                    {
+                        WriteToFile("Failed to find cube: " + cubeNames[i]);
+                    }
+                    else
+                    {
+                    WriteToFile("Found cube: " + sphereNames[i]);
+                    initialPositions[i] = cubes[i].transform.position;
+                    lastRecordedPosition[i] = initialPositions[i];
+                    distance[i] = Vector3.Distance(lastRecordedPosition[i], initialPositions[i]);
+                    //WriteToFile("Found cube: " + cubeNames[i]);
+                }
+                }
+
+                CheckAssignments(spheres, "spheres");
+                CheckAssignments(cubes, "cubes");
+                
+                //AllSpheresOff();
+        }
+            
+        timer = toggleInterval;
+        // Start the color change routine
+        InvokeRepeating("ChangeColor", 3, 3);
+    }
+
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            //ToggleVisibility(spheres[0]);
+            timer = toggleInterval; // Reset the timer
+        }
+        CheckCubeMovements();
+    }
+
+    // Call this method to move to the next sphere and cube setup
+    public void NextSetup()
+    {
+        UpdateSphereAndCube();
+    }
+
+
+    // Function to check the movement of each cube from its initial position
+    private void CheckCubeMovements()
+    {
+        for (int i = 0; i < spheres.Length; i++)
+        { 
+            if (spheres[i] != null)
+            {
+                string data = "";
+                lastRecordedPosition[i] = cubes[i].transform.position;
+                distance[i] = Vector3.Distance(lastRecordedPosition[i], initialPositions[i]);
+                if (lastRecordedPosition[i] != initialPositions[i])
+                {
+                    if (distance[i] > distanceThreshold)
+                    {
+                        if (waitingForInteraction)
+                        {
+                            data = "At: " + GetCurrentTime();
+                            data += cubes[i];
+                            //data += " moved to " + lastRecordedPosition[i];
+                            data += " moved and covered distance = " + distance[i];
+                            WriteToFile(data);
+                            //WriteToFile("Sphere " + spheres[i].name + " has moved significantly from its original position.");
+                            RecordInteractionStart();
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    // Function to record interaction start time
+    private void RecordInteractionStart()
+    {
+        // Increment interaction count
+        interactionCount++;
+        waitingForInteraction = false;
+    }
+
+    private void ChangeColor()
+    {
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            if (cubes[i] != null)
+            {
+                cubes[i].transform.position = initialPositions[i];
+            }
+        }
+                waitingForInteraction = true;
+        //buttonindexrecorded = false
+        UpdateSphereAndCube();
+    }
+
+    public void UpdateSphereAndCube()
+    {
+        if (currentSphereIndex >= sphereColorSequence.Length) currentSphereIndex = 0;
+
+        // Enable only the sphere corresponding to the current index
+        int activeSphereIndex = sphereColorSequence[currentSphereIndex];
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            if(i == activeSphereIndex)
+            {
+                WriteToFile(spheres[i].name + " appeared at " + GetCurrentTime());
+            }
+            spheres[i].SetActive(i == activeSphereIndex);
+        }
+
+        // Update the cube highlight, changing every four spheres
+        int cubeIndex = (currentSphereIndex / 3) % cubes.Length;
+        if (lastprintindex != "\"Sphere index: \" + activeSphereIndex + \" Cube index: \" + cubeIndex")
+        {
+            WriteToFile("Sphere index: " + activeSphereIndex + " Cube index: " + cubeIndex);
+            lastprintindex = "Sphere index: " + activeSphereIndex + " Cube index: " + cubeIndex;
+        }
+        /*for(int j = 0; j < cubes.Length; j++)
+        {
+            if (j == cubeIndex)
+            {
+                if (waitingForInteraction)
+                {
+                    if (lastprintbutton != cubes[j].name + " appeared at ")
+                    {
+                        WriteToFile(cubes[j].name + " appeared at " + GetCurrentTime());
+                        lastprintbutton = cubes[j].name + " appeared at ";
+                    }
+                   
+                cubes[j].SetActive(true);
+                }
+            }
+            else
+            {
+                // Getting the current color of the material
+                cubes[j].SetActive(false);
+            }   
+            //cube.GetComponent<Renderer>().material.color = Color.black; // Default to unhighlighted
+        } */
+        
+        //cubes[cubeIndex].GetComponent<Renderer>().material.color = spheres[activeSphereIndex].GetComponent<Renderer>().material.color; // Highlight the cube
+
+        currentSphereIndex++;
+    }
+
+
+
+    private void ToggleVisibility(GameObject sphere)
+    {
+        
+        AllSpheresOff();
+        /*
+         * // Toggle the Renderer.enabled state for all children
+        foreach (Transform child in transform)
+        {
+            Renderer childRenderer = child.GetComponent<Renderer>();
+            if (childRenderer != null)
+            {
+                childRenderer.enabled = !childRenderer.enabled;
+            }
+        }
+        */
+        // Activate the selected sphere
+        sphere.SetActive(true);
+    }
+
+    // Helper method to check if all elements in an array are assigned
+    private void CheckAssignments(GameObject[] array, string arrayName)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == null)
+            {
+                WriteToFile("Assignment missing in " + arrayName + " at index: " + i);
+            }
+            else
+            {
+                //WriteToFile(arrayName + " at index " + i + " is assigned to " + array[i].name);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Record game end time and interaction count
+
+        string data = "Game ended at: " + GetCurrentTime();
+        data += "Total interactions: " + interactionCount + "\n";
+        WriteToFile(data);
+        // Close the stream writer
+        trailStreamWriter.Close();
+    }
+
+   
+    // Function to turn off all spheres
+    private void AllSpheresOff()
+    {
+        foreach (GameObject sphere in spheres)
+        {
+            if (sphere != null) // Check if the sphere reference is not null
+                sphere.SetActive(false);
+            else
+                WriteToFile("One of the sphere objects is not assigned in the array.");
+        }
+    }
+
+    // Function to turn off all buttons
+    private void AllButtonsOff()
+    {
+        foreach (GameObject button in cubes)
+        {
+            if (button != null) // Check if the button reference is not null
+                button.SetActive(false);
+            else
+                WriteToFile("One of the button objects is not assigned in the array.");
+        }
+    }
+
+    private void WriteToFile(string data)
+    {
+        if (trailStreamWriter != null)
+        {
+            try
+            {
+                trailStreamWriter.WriteLine(data);
+                trailStreamWriter.Flush();
+                UnityEngine.Debug.Log(data);
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError("Error writing to file: " + e.Message);
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("StreamWriter is null.");
+        }
+    }
+
+    private float GetCurrentTime()
+    {
+        return CalculateTimeDifference(appstartTime, DateTime.Now.ToString("mm:ss.fff"));
+    }
+
+
+    public static float CalculateTimeDifference(string timestamp1Str, string timestamp2Str)
+    {
+
+        DateTime timestamp1 = DateTime.ParseExact(timestamp1Str, "mm:ss.fff", CultureInfo.InvariantCulture);
+        DateTime timestamp2 = DateTime.ParseExact(timestamp2Str, "mm:ss.fff", CultureInfo.InvariantCulture);
+
+        TimeSpan timeDifference = timestamp2 - timestamp1;
+        float timeDifferenceInSeconds = (float)timeDifference.TotalMilliseconds / 1000.0f;
+
+        return timeDifferenceInSeconds;
+    }
+
+    private GameObject GetChildWithName(GameObject parent, string name)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            if (child.gameObject.name == name)
+            {
+                return child.gameObject;
+            }
+        }
+        return null;
+    }
+}
+
